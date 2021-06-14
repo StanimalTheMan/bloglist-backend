7ab7367502d22bc82bbbaac2ef2bb77353d5a4ee
StanimalTheMan <sjc591@nyu.edu>
2021-06-14 15:12:06 -0400

    helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const sumLikes = (sum, item) => {
+    return sum + item.likes;
+  };
+
+  return blogs.reduce(sumLikes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
