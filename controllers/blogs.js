9e720d711a9a04a406febac2af05d7a779ed6ebf
StanimalTheMan <sjc591@nyu.edu>
2021-06-18 16:33:53 -0400

    working on implementing deleting and updating blog post resource

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -35,4 +35,25 @@ blogsRouter.post("/", async (request, response) => {
   response.json(savedBlog);
 });
 
+blogsRouter.delete("/:id", (request, response, next) => {
+  Blog.findByIdAndRemove(request.params.id);
+});
+
+blogsRouter.update("/:id", (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
+});
+
 module.exports = blogsRouter;
